29671b02f55e3d5631f3644ab6491e0cc540b3df
kosintsev <andrejkosincev@gmail.com>
2022-10-14 20:48:30 +0700

    selected unique records from the array
    issue #1

--- index.php
+++ index.php
@@ -1,1 +1,19 @@
 <?php
+$array = [
+    ['id' => 1, 'date' => "12.01.2020", 'name' => "test1"],
+    ['id' => 2, 'date' => "02.05.2020", 'name' => "test2"],
+    ['id' => 4, 'date' => "08.03.2020", 'name' => "test4"],
+    ['id' => 1, 'date' => "22.01.2020", 'name' => "test1"],
+    ['id' => 2, 'date' => "11.11.2020", 'name' => "test4"],
+    ['id' => 3, 'date' => "06.06.2020", 'name' => "test3"],
+];
+
+$uniqValuesArray = [];
+$column = 'id';
+$tmp = array_filter($array, fn($a) => key_exists($column, $a));
+$tmp = array_map(fn($a) => $a[$column], $tmp);
+$uniqValuesColumn = array_unique($tmp);
+array_walk($uniqValuesColumn, function ($v, $k) use (&$uniqValuesArray, $array) {
+    $uniqValuesArray[$k] = $array[$k];
+});
+print_r($uniqValuesArray);
