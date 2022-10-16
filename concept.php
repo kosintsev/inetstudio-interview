87c1213d65f4f608c5380aa8466880393357bfd5
kosintsev <andrejkosincev@gmail.com>
2022-10-16 22:08:22 +0700

    strategy storage token
    issue #9

--- concept.php
+++ concept.php
@@ -1,9 +1,11 @@
 <?php
 class Concept {
     private $client;
+    private $storage;
 
     public function __construct() {
         $this->client = new \GuzzleHttp\Client();
+        $this->storage = new \RedisStorage();
     }
 
     public function getUserData() {
@@ -19,4 +21,42 @@ class Concept {
 
         $promise->wait();
     }
+
+    private function getSecretKey(): string {
+        return $this->storage->get('token')??'';
+    }
+}
+
+interface StorageStrategy
+{
+    public function save($fieldName);
+    public function get($fieldName);
+}
+
+class RedisStorage implements StorageStrategy
+{
+    public function __construct() {}
+    public function save($fieldName) {}
+    public function get($fieldName) {}
+}
+
+class FileStorage implements StorageStrategy
+{
+    public function __construct() {}
+    public function save($fieldName) {}
+    public function get($fieldName) {}
+}
+
+class DbStorage implements StorageStrategy
+{
+    public function __construct() {}
+    public function save($fieldName) {}
+    public function get($fieldName) {}
+}
+
+class CloudStorage implements StorageStrategy
+{
+    public function __construct() {}
+    public function save($fieldName) {}
+    public function get($fieldName) {}
 }
