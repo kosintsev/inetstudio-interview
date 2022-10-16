777c296a60949c047c8917a628097739758b018c
kosintsev <andrejkosincev@gmail.com>
2022-10-16 19:36:53 +0700

    abstractions must not depend on details
    issue #8

--- solid_d.php
+++ solid_d.php
@@ -2,22 +2,31 @@
 
 abstract class XMLHttpService extends XMLHTTPRequestService {
     private XMLHttpService $service;
-    public abstract function post(string $url);
-    public abstract function get(string $url, array $options);
+    public abstract function request(string $url);
 }
 
-class Http extends XMLHttpService {
+class HttpPostRequest extends XMLHttpService {
     private $service;
 
     public function __construct(XMLHttpService $xmlHttpService) {
         $this->service = $xmlHttpService;
     }
 
-    public function get(string $url, array $options) {
-        $this->service->request($url, 'GET', $options);
+    public function request(string $url) {
+        $this->service->request($url, 'POST');
+    }
+}
+
+class HttpGetRequest extends XMLHttpService {
+    private $service;
+    private $options;
+
+    public function __construct(XMLHttpService $xmlHttpService, array $options) {
+        $this->service = $xmlHttpService;
+        $this->options = $options;
     }
 
-    public function post(string $url) {
-        $this->service->request($url, 'GET');
+    public function request(string $url) {
+        $this->service->request($url, 'GET', $this->options);
     }
 }
