dda5d75464c1376a933cc666b3880e926390f2ef
kosintsev <andrejkosincev@gmail.com>
2022-10-16 19:31:46 +0700

    top level depends of abstractions
    issue #8

--- solid_d.php
+++ solid_d.php
@@ -1,11 +1,17 @@
 <?php
 
-class XMLHttpService extends XMLHTTPRequestService {}
+abstract class XMLHttpService extends XMLHTTPRequestService {
+    private XMLHttpService $service;
+    public abstract function post(string $url);
+    public abstract function get(string $url, array $options);
+}
 
-class Http {
+class Http extends XMLHttpService {
     private $service;
 
-    public function __construct(XMLHttpService $xmlHttpService) { }
+    public function __construct(XMLHttpService $xmlHttpService) {
+        $this->service = $xmlHttpService;
+    }
 
     public function get(string $url, array $options) {
         $this->service->request($url, 'GET', $options);
